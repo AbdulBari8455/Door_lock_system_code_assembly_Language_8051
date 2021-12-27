c23944a5be83a69e05d44f934704be0912fa4eb1
AbdulBari <73050746+AbdulBari8455@users.noreply.github.com>
2021-12-27 01:25:14 -0800

    Update Serial Communication between two MicroControllers.asm

--- Serial Communication between two MicroControllers.asm
+++ Serial Communication between two MicroControllers.asm
@@ -1,13 +1,13 @@
-/*You are required to write a program to communicate 
-two 89C52 microcontrollers serially at a 9600 baud rate. 
+/* You are required to write a program to communicate 
+two 89C52 microcontrollers serially at a 9600 baud rate 
 Connect eight SPDT switches with port P1 and a 
-push button with P3.2 of microcontroller-1. These 
-eight SPDT switches represent a byte. 
+push button with P3.2 of microcontroller-1 These 
+eight SPDT switches represent a byte 
 Whenever the push button is pressed and then released,
 the corresponding byte is transmitted to the 2nd 
-microcontroller. The microcontroller-2 displays the 
+microcontroller The microcontroller-2 displays the 
 received string on Hyper Terminal. Assume oscillator 
-frequency to be 11.0592MHz.*/ 
+frequency to be 11.0592MHz */ 
 
 ;Code
 ;transmitter
